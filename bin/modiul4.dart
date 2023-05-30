9a0d514db95dd9f1492ef257c51b74fcb50fb4fc
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2023-05-30 17:26:41 +0600

    modiul4

---
+++ bin/modiul4.dart
@@ -0,0 +1,16 @@
+//class 41
+class father {
+  Bapertaka (){print("Mot taka= 2000000");}
+  }
+class sun extends father{
+  Bapertaka (){print("Mot taka= 3000000");}
+}
+
+void main(){
+  //class 41
+  sun Sunt= sun();
+  Sunt.Bapertaka();
+
+  father Fathert =father();
+  Fathert.Bapertaka();
+}
---
+++ test/modiul4_test.dart
@@ -0,0 +1,8 @@
+import 'package:modiul4/modiul4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
